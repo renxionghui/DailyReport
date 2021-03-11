4ab4585da1938a44463482a2212984944b491e3a
renxionghui <renxionghui@126.com>
2021-03-11 18:04:51 +0800

    style:修改UI风格

--- components/personal-panel/index.js
+++ components/personal-panel/index.js
@@ -1,18 +1,24 @@
 // components/personal/index.js
 Component({
+  options:{
+    addGlobalClass: true
+  },
   properties: {},
 
   data: {
     nickName: '点击登陆',
-    avatarUrl: './images/avatar.svg'
+    avatarUrl: './images/avatar.svg',
+    name:'',
+    phone:'',
+    department:'',
+    percentage:0
   },
   lifetimes: {
     created() {
-      console.log('created')
       const that = this;
+      //从服务器获取用户信息
       wx.getUserInfo({
         success(res) {
-          console.log(res)
           const {
             userInfo: {
               nickName,
@@ -24,6 +30,22 @@ Component({
             avatarUrl
           })
         }
+      })
+    },
+    attached() {
+      let infoCount = 0;
+      const userStorage = wx.getStorageSync('user');
+      console.log(userStorage)
+      const name = userStorage.name;
+      if(name) infoCount++
+      const phone = userStorage.phone;
+      if(phone) infoCount++
+      const department = userStorage.department;
+      if(department) infoCount++
+      const percentage = infoCount/3 * 100;
+      console.log(percentage)
+      this.setData({
+        name,phone,department,percentage
       })
     }
   },
@@ -39,6 +61,27 @@ Component({
         nickName,
         avatarUrl
       })
-    }
+    },
+    handleSave(){
+      const {name, phone, department} = this.data;
+      wx.setStorageSync('user',{
+        name,phone,department
+      })
+    },
+    handleNameChange({detail}){
+      this.setData({
+        name:detail
+      })
+    },
+    handlePhoneChange({detail}){
+      this.setData({
+        phone:detail
+      })
+    },
+    handleDepartmentChange({detail}){
+      this.setData({
+        department:detail
+      })
+    },
   }
 })
